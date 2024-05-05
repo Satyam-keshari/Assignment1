da1a62b8c9e4caa0a57084ed03935f75ae6c0e07
Satyam-keshari <153010983+Satyam-keshari@users.noreply.github.com>
2024-05-05 14:12:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.querySelector("#show-login").addEventListener("click",function(){
+//      document.querySalector(".popup").classList.add("active");
+// });
+
+// document.querySelector(".popup .close-btn").addEventListener("click",function(){
+//     document.querySalector(".popup").classList.remove("active");
+// });
